acbc71631b98a21ef1be0e03137050fa30f17eaa
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2026-05-18 12:56:23 +0900

    Create level_editor.py
    01_03完了

---
+++ level_editor.py
@@ -0,0 +1,27 @@
+import bpy
+
+# blenderに登録するアドオン情報 
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "kamata",
+    "version": (1,0),
+    "blender": (3,3,1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
+# アドオン有効化時コールバック
+def register():
+    print("レベルエディタが有効化されました。")
+
+# アドオン無効化時コールバック
+def unregister():
+    print("レベルエディタが無効化されました。")
+
+# テスト実行用コード
+if __name__ == "__main__":
+    register()
